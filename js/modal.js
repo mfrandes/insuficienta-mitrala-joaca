a6bcbdad91b08fa2eb7233bbe88d41234230fb18
Mircea <frandesmyrce@gmail.com>
2018-11-22 23:59:30 +0200

    contact.. to be continued..:)

--- js/modal.js
+++ js/modal.js
@@ -22,25 +22,4 @@ function closeModal() {
     $("modal1").style.display = "none";
 }
 
-function submit() {
-    var at = $("e-mail").value.indexof("@");
-    var fname = $("fName").value;
-    var lname = $("lName").value;
-    submitOK= "true";
-    
-    if (at == -1) {
-        alert("Not a valid e-mail!");
-        submitOK = "false";
-    }
-    if (fname.length <= 2) {
-        alert("Nume invalid");
-        submitOK = "false";
-    }
-    if (lname.length <= 2) {
-        alert("Nume invalid");
-        submitOK = "false";
-    }
-    if (submitOK == "false") {
-        return false;
-    }
-}
+
